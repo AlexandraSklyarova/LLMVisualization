2bb279c8c70964d74637795f83cfa0fa279f7aba
AlexandraSklyarova <alexandra.sklyarova09@gmail.com>
2025-06-13 15:32:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ for row_types in chunks(types, 3):
             ).encode(text=alt.Text("Score:Q", format=".2f"))
         ).properties(
             title=t,
-            width=350,
+            width=450,
             height=600
         )
 
