bc4fad3849c6a713dc01be558e8456f21ac0dd5c
AlexandraSklyarova <alexandra.sklyarova09@gmail.com>
2025-06-13 13:20:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -143,14 +143,14 @@ def polar_positions(n, radius_step=1.5):
     y = [r * np.sin(a) for r, a in zip(radii, angles)]
     return x, y
 
-agg_df["x"], agg_df["y"] = polar_positions(len(agg_df), radius_step=0.3)
+agg_df["x"], agg_df["y"] = polar_positions(len(agg_df), radius_step=0.2)
 
 
 # --- BUBBLE CHART ---
 bubbles = alt.Chart(agg_df).mark_circle(opacity=0.8).encode(
     x=alt.X("x:Q", axis=None),
     y=alt.Y("y:Q", axis=None),
-    size=alt.Size("Size:Q", scale=alt.Scale(range=[2000, 25000]), legend=None),
+    size=alt.Size("Size:Q", scale=alt.Scale(range=[4000, 45000]), legend=None),
     color=alt.Color("Type:N", legend=alt.Legend(title="Model Type")),
     tooltip=[
         alt.Tooltip("Type:N", title="Model Type"),
